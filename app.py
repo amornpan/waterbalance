12edff797a24409f1d05ca5d22858251173ccccc
amornpan phornchaicharoen <amornpan@gmail.com>
2023-09-05 09:39:44 +0700

    setting config file

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ import plotly.express as px
 import geopandas as gpd
 
 app = Flask(__name__)
+# Load configuration from config.py
+app.config.from_pyfile('config.py')
 Bootstrap(app)
+
+# Access configuration values
+secret_key = app.config['SECRET_KEY']
+database_uri = app.config['DATABASE_URI']
+db_username = app.config['DB_USERNAME']
+db_password = app.config['DB_PASSWORD']
+
 
 @app.route('/moban_map')
 def moban_map():
     # Read the shapefile
-    gdf_mooban = gpd.read_file('static/boundary_mooban_ll/boundary_mooban_ll.shp', encoding='cp874')
+    gdf_mooban = gpd.read_file(
+        'static/boundary_mooban_ll/boundary_mooban_ll.shp', encoding='cp874')
 
     # Create a map with Plotly Express and Mapbox for gdf_tambon
     fig = px.choropleth_mapbox(
@@ -18,7 +28,8 @@ def moban_map():
         locations=gdf_mooban.index,
         color=gdf_mooban['MOO_NAME'],
         title="Interactive Map for gdf_mooban",
-        center={"lat": gdf_mooban.geometry.centroid.y.mean(), "lon": gdf_mooban.geometry.centroid.x.mean()},
+        center={"lat": gdf_mooban.geometry.centroid.y.mean(
+        ), "lon": gdf_mooban.geometry.centroid.x.mean()},
         mapbox_style="carto-positron",
         zoom=10
     )
@@ -32,11 +43,13 @@ def moban_map():
         autosize=True,
         # width=figure_width,
         # height=figure_height,
-        margin={"r": 0, "t": 30, "l": 0, "b": 0}  # Add margin to accommodate title and other elements
+        # Add margin to accommodate title and other elements
+        margin={"r": 0, "t": 30, "l": 0, "b": 0}
     )
 
     # Render the map on the HTML template
     return render_template('mooban_map.html', plot=fig.to_html(full_html=False))
+
 
 if __name__ == '__main__':
     host = '113.53.253.56'
